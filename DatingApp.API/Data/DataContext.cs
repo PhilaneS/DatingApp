3717f3ad8c6fdbaef6e1f70a2b452b887a5f154a
PhilaneS <PhilaneS@ZAZN-DEV-001>
2019-08-15 01:58:46 +0200

    initial commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,20 @@
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options): base(options){}
+
+        // protected override void OnModelCreating(ModelBuilder builder)
+        // {
+        //     base.OnModelCreating(builder);
+        //     // Customize the ASP.NET Identity model and override the defaults if needed.
+        //     // For example, you can rename the ASP.NET Identity table names and more.
+        //     // Add your customizations after calling base.OnModelCreating(builder);
+        // }
+        
+        public DbSet<Value> Values { get; set; }
+    }
+}

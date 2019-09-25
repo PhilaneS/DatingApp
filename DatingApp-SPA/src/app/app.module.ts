a2ef730c4166524f4704ddb783deb90c389e7a51
PhilaneS <PhilaneS@ZAZN-DEV-001>
2019-09-25 17:25:49 +0200

    WIP : JWT implemetnation

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -2,13 +2,18 @@ import { AuthService } from './_services/auth.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule } from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
-import {FormsModule} from '@angular/forms';
 import { HomeComponent } from './Home/Home.component';
 import { RegisterComponent } from './register/register.component';
+// import {ErrorInterceptorProvider} from ''
+
+import { AlertifyService } from './_services/alertify.service';
+
 
 @NgModule({
    declarations: [
@@ -21,10 +26,13 @@ import { RegisterComponent } from './register/register.component';
       BrowserModule,
       AppRoutingModule,
       HttpClientModule,
-      FormsModule
+      FormsModule,
+      BsDropdownModule.forRoot()
    ],
    providers: [
-      AuthService
+      AuthService,
+      // ErrorInterceptorProvider,
+      AlertifyService
    ],
    bootstrap: [
       AppComponent
